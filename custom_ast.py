2e3ef777fe183787037f55faf9f7d1c9fa3bf6cb
evan <evan@liukersun.com>
2024-05-23 14:21:13 +0800

    feat:支持列表计算.

--- custom_ast.py
+++ custom_ast.py
@@ -1,6 +1,5 @@
 from custom_tokenize import TokenType, Token
 from loguru import logger
-from custom_func_config import FUNC_ARGS
 
 
 class Node:
@@ -30,50 +29,87 @@ def build_syntax_tree(tokens):
             current_node = syntax_tree[-1]
         elif token.token_type == TokenType.Split:
             current_node = syntax_tree[-1]
-
-    logger.info(syntax_tree)
     return root.children[0]
 
 
 def calculate_tree_value(node, data):
     match node.token.token_type:
         case TokenType.Function:
-            # 取出函数配置
-            func_config = FUNC_ARGS[node.token.value]
             match node.token.value:
                 case "SUM":
-                    # 判断sum的每一个参数.
-                    # 如果有多个列表,需要判断列表长度是否一致.
-                    # 如果没有列表,则返回数字,如果只有一个列表,则返回列表,·
-                    sum_list = [
+                    args_list = [
                         calculate_tree_value(child, data) for child in node.children
                     ]
-                    list_index_list = []
-                    list_length_list = []
-                    for item_index in range(len(sum_list)):
-                        if isinstance(sum_list[item_index], list):
-                            list_index_list.append(item_index)
-                            list_length_list.append(len(sum_list[item_index]))
-                    if len(list_index_list) > 1:
-                        ...
-
-                    # if all(isinstance(item, (int, float)) for item in sum_list):
-                    #     return sum(sum_list)
-                    # else:
-                    #     list_count = 0
-                    #     list_length = set()
-                    #     for item in sum_list:
-                    #         if isinstance(item, list):
-                    #             list_count += 1
-                    #             list_length.add(len(item))
-                    #     if list_count > 1 and len(list_length) > 1:
-                    #         raise ValueError("ERROR! 传入了多个长度不同列表!")
-                    #     else:
-                    #         if list_count == 1:
-                    #             # 找到列表元素所在位置
-                    #             return [item for item in sum_list]
-                    #         else:
-                    #             return [sum(item) for item in zip(*sum_list)]
+                    a, b = args_list
+                    # 如果a和b都是列表，且长度相同，则逐元素相加
+                    if isinstance(a, list) and isinstance(b, list) and len(a) == len(b):
+                        return [x + y for x, y in zip(a, b)]
+                    # 如果a是整数，b是列表，则将a加到b的每个元素上
+                    elif isinstance(a, (int, float)) and isinstance(b, list):
+                        return [x + a for x in b]
+                    # 如果b是整数，a是列表，则将b加到a的每个元素上
+                    elif isinstance(b, (int, float)) and isinstance(a, list):
+                        return [x + b for x in a]
+                    # 如果两者都是整数，则直接相加
+                    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+                        return a + b
+                    else:
+                        raise ValueError(
+                            "Unsupported input types or list lengths do not match."
+                        )
+                case "SUB":
+                    args_list = [
+                        calculate_tree_value(child, data) for child in node.children
+                    ]
+                    a, b = args_list
+                    if isinstance(a, list) and isinstance(b, list) and len(a) == len(b):
+                        return [x - y for x, y in zip(a, b)]
+                    elif isinstance(a, (int, float)) and isinstance(b, list):
+                        return [a - x for x in b]
+                    elif isinstance(b, (int, float)) and isinstance(a, list):
+                        return [x - b for x in a]
+                    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+                        return a - b
+                    else:
+                        raise ValueError(
+                            "Unsupported input types or list lengths do not match."
+                        )
+                case "MUL":
+                    args_list = [
+                        calculate_tree_value(child, data) for child in node.children
+                    ]
+                    a, b = args_list
+                    if isinstance(a, list) and isinstance(b, list) and len(a) == len(b):
+                        return [x * y for x, y in zip(a, b)]
+                    elif isinstance(a, (int, float)) and isinstance(b, list):
+                        return [x * a for x in b]
+                    elif isinstance(b, (int, float)) and isinstance(a, list):
+                        return [x * b for x in a]
+                    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+                        return a * b
+                    else:
+                        raise ValueError(
+                            "Unsupported input types or list lengths do not match."
+                        )
+                case "DIV":
+                    args_list = [
+                        calculate_tree_value(child, data) for child in node.children
+                    ]
+                    a, b = args_list
+                    if isinstance(a, list) and isinstance(b, list) and len(a) == len(b):
+                        return [x / y if y != 0 else float("inf") for x, y in zip(a, b)]
+                    elif isinstance(a, (int, float)) and isinstance(b, list):
+                        return [a / x if x != 0 else float("inf") for x in b]
+                    elif isinstance(b, (int, float)) and isinstance(a, list):
+                        return [b / x if x != 0 else float("inf") for x in a]
+                    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+                        if b == 0:
+                            raise ZeroDivisionError("Cannot divide by zero.")
+                        return a / b
+                    else:
+                        raise ValueError(
+                            "Unsupported input types or list lengths do not match."
+                        )
                 case "AVG":
                     return sum(
                         calculate_tree_value(child, data) for child in node.children
--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from custom_tokenize import tokenize
 from custom_ast import build_syntax_tree, calculate_tree_value
 from loguru import logger
 
-formula = "SUM(SUM(1,2),step2.aa.bb)"
-data = {"step1": {"aa": {"bb": [1, 2]}}, "step2": {"aa": {"bb": [1, 2, 3]}}}
+data_dict = {"step1": {"aa": {"bb": [1, 2]}}, "step2": {"aa": {"bb": [1, 2, 3]}}}
+formula1 = "SUM(SUM(3,2),step2.aa.bb)"
+formula2 = "SUB(SUB(1,2),step2.aa.bb)"
+formula3 = "MUL(MUL(1,2),step2.aa.bb)"
+formula4 = "DIV(DIV(1,2),step2.aa.bb)"
 
-tokens = tokenize(formula)
-syntax_tree = build_syntax_tree(tokens)
-result = calculate_tree_value(syntax_tree, data)
 
-logger.info(rf"formula : {formula}")
-logger.info(rf"Result : {result}")
+def parser(formula, data):
+    tokens = tokenize(formula)
+    syntax_tree = build_syntax_tree(tokens)
+    result = calculate_tree_value(syntax_tree, data)
+    logger.info(rf"formula:{formula},result: {result}")
+    return result
+
+
+logger.info(rf"data_dict is :{data_dict}")
+parser(formula=formula1, data=data_dict)
+parser(formula=formula2, data=data_dict)
+parser(formula=formula3, data=data_dict)
+parser(formula=formula4, data=data_dict)
